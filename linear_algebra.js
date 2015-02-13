bfe1845d9067caa548e9d6316a190f7ea275d775
unknown <djacosta@ITI-1109L.cityofno.com>
2015-02-13 15:22:08 -0600

    subtract

--- linear_algebra.js
+++ linear_algebra.js
@@ -1,17 +1,33 @@
-add_vectors = function(args){
+
+//vectors can be any number of dimensions (R2 - 2-tuples; R3 - 3-tuples, etc)
+add_vectors = function(vectors){
   var product = [];
-  product.length = args[0].length;
+  product.length = vectors[0].length;
   for (var v=0;v<product.length;v++){
     product[v] = 0;
   }
-  for (var i=0;i<args.length;i++){
-    for (var v=0;v<args[i].length;v++){
-      product[v] = product[v] + args[i][v];
+  for (var i=0;i<vectors.length;i++){
+    for (var v=0;v<vectors[i].length;v++){
+      product[v] = product[v] + vectors[i][v];
     }
   }
   return product
 }
 
+subtract_vectors = function(vectors){
+  var product = [];
+  for (var v=0;v<vectors[0].length;v++){
+    product[v] = vectors[0][v];
+  }
+  for (var i=1;i<vectors.length;i++){
+    for (var v=0;v<vectors[i].length;v++){
+      product[v] = product[v] - vectors[i][v];
+    }
+  }
+  return product;
+}
+
+//Scalars scale the size of the vector
 multiply_vector_by_scalar = function(vector, scalar){
   var product = [];
   for (var i=0;i<vector.length;i++){
